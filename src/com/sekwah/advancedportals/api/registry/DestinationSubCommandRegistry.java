0b2649891ef7227796f0a984545b57a091280dca
Alastair Hawkes <alastair.hawkes@gmail.com>
2017-01-01 04:16:58 +0000

    Forgot the desti

--- src/com/sekwah/advancedportals/api/registry/DestinationSubCommandRegistry.java
+++ src/com/sekwah/advancedportals/api/registry/DestinationSubCommandRegistry.java
@@ -5,5 +5,5 @@ package com.sekwah.advancedportals.api.registry;
  *
  * @author sekwah41
  */
-public class DestinationSubCommandRegistry {
+public class DestinationSubCommandRegistry extends SubCommandRegistry {
 }
--- src/com/sekwah/advancedportals/api/registry/PortalSubCommandRegistry.java
+++ src/com/sekwah/advancedportals/api/registry/PortalSubCommandRegistry.java
@@ -1,7 +1,5 @@
 package com.sekwah.advancedportals.api.registry;
 
-import com.sekwah.advancedportals.api.commands.SubCommand;
-
 /**
  * Designed to let addons add new command sections to access, edit or add new functonality.
  *
@@ -9,12 +7,6 @@ import com.sekwah.advancedportals.api.commands.SubCommand;
  */
 public class PortalSubCommandRegistry extends SubCommandRegistry {
 
-    /**
-     * @param subCommand
-     * @return if the subcommand is registered or not
-     */
-    public static boolean registerSubCommand(SubCommand subCommand){
-        return false;
-    }
+
 
 }
